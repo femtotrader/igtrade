e7301c016247f77a06a74bdad95e26cbf593bd33
Stéphane Planquart <stephane@planquart.com>
2015-06-21 08:29:07 +0200

    remove some stuff

--- personal.py
+++
@@ -1,7 +0,0 @@
-username = u'splanquartdemo'
-password = 'U3RlcGhhbmUxNjk=\n'
-is_demo = True
-proxies = {'https': u''}
-epic = 'IX.D.DAX.IMF.IP'
-account_nb = u'0'
-api_key = u'b7c2876ef9f3fed04769c374596cd17ef2108da3'
